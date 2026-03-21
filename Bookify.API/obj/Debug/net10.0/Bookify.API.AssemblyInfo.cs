788a60d87a3271c393f4aff5222c36f0679fbb4f
Peter Dinis <petter1@pobx.sk>
2026-03-21 18:57:31 +0100

    fix: Fixing some backend things

--- Bookify.API/obj/Debug/net10.0/Bookify.API.AssemblyInfo.cs
+++ Bookify.API/obj/Debug/net10.0/Bookify.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ce30265cf016150e5a482acd8976233a047077f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+582730926ef59ca82501d10afae2098264822039")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
