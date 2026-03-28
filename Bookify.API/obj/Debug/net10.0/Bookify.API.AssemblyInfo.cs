a1ca084e6e082cf0c83780ce5444b28c4ab4ddb2
Peter Dinis <petter1@pobx.sk>
2026-03-28 14:24:42 +0100

    feat: Start to connected backend and frontend

--- Bookify.API/obj/Debug/net10.0/Bookify.API.AssemblyInfo.cs
+++ Bookify.API/obj/Debug/net10.0/Bookify.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+788a60d87a3271c393f4aff5222c36f0679fbb4f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6bc44657e8c31e6d76c903d3100734cfc426d5cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
